22fc779dae66c49740590eda53b55c8093651335
jeroen van der zwam <jeroenvanderzwam@hotmail.com>
2023-12-13 15:31:35 +0100

    refactoring ReturnsSelf

--- main/java/returnsSelf/ReturnsSelf.java
+++ main/java/returnsSelf/ReturnsSelf.java
@@ -4,40 +4,55 @@ import export.File;
 import factexporter.DataFlowGraphService;
 import factexporter.DecompilationService;
 import facts.Fact;
+import sourcecode.Func;
 import thispointer.ThisPointer;
+import thispointer.ThisPointer.ThisPointerRegister;
 
 public class ReturnsSelf implements Fact 
 {
-	private DecompilationService dataService;
-	private DataFlowGraphService graphService;
+	private final DecompilationService dataService;
+	private final DataFlowGraphService graphService;
+	private final ThisPointerRegister thisPointerRegister;
 	
 	public ReturnsSelf(DecompilationService dataServ, DataFlowGraphService graphServ)
 	{
 		dataService = dataServ;
 		graphService = graphServ;
+		var compilerSpec = dataService.compilerSpec();
+		thisPointerRegister = new ThisPointer().build(compilerSpec);
 	}
 
 	public void CreateFacts(File file) {
-		var compilerSpec = dataService.compilerSpec();
 		file.open();
-		var thisPointerRegister = new ThisPointer().build(compilerSpec);
 		for (var function : dataService.functions())
 		{
-			if (function.isThunk()) { continue; }
-			if (!function.hasParameters()) { continue ;}
-			
-			var param = function.parameters().get(0);
-			if (param.inRegister()) {
-				if (param.register().name().equals(thisPointerRegister.name())) {
-					graphService.buildGraph(function);
-					
-					if (graphService.pathFromParamToReturn(param)) {
-						var output = String.format("returnsSelf(%s).", function.address());
-						file.write(output);
-					}
-				}
+			if (functionReturnsSelf(function)) {
+				var output = String.format("returnsSelf(%s).", function.address());
+				file.write(output);
 			}
 		}
 		file.close();
+	}
+	
+	private boolean functionReturnsSelf(Func function) {
+		if (function.isThunk()) { return false; }
+		if (!function.hasParameters()) { return false ;}
+		if (firstParamHasPathToReturn(function)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean firstParamHasPathToReturn(Func function) {
+		var param = function.parameters().get(0);
+		if (param.inRegister()) {
+			if (param.register().name().equals(thisPointerRegister.name())) {
+				graphService.buildGraph(function);
+				if (graphService.pathFromParamToReturn(param)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
