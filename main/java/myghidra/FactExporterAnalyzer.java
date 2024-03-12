bf2ae0f2cc47298cddd0762a5863e6e76a2a82f2
jeroen van der zwam <jeroenvanderzwam@hotmail.com>
2024-03-12 10:16:06 +0100

    readme toegevoegd, en file option toegevoegd

--- main/java/myghidra/FactExporterAnalyzer.java
+++ main/java/myghidra/FactExporterAnalyzer.java
@@ -15,7 +15,10 @@
  */
 package myghidra;
 
+import java.io.File;
 import java.util.regex.Pattern;
+
+import com.kenai.jffi.ObjectParameterInfo.ObjectType;
 
 import factexporter.FactExporter;
 import factexporter.adapters.GhidraDataFlowAdapter;
@@ -24,6 +27,7 @@ import factexporter.export.TextFile;
 import ghidra.app.services.AbstractAnalyzer;
 import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
+import ghidra.framework.options.OptionType;
 import ghidra.framework.options.Options;
 import ghidra.program.model.address.AddressSetView;
 import ghidra.program.model.listing.Program;
@@ -34,52 +38,45 @@ import ghidra.util.task.TaskMonitor;
  * TODO: Provide class-level documentation that describes what this analyzer does.
  */
 public class FactExporterAnalyzer extends AbstractAnalyzer {
+	
+	private static final String FILEPATH_OPTION = "ExportPath";
+	File exportFile;
 
 	public FactExporterAnalyzer() {
-
-		// TODO: Name the analyzer and give it a description.
-
 		super("Fact exporter Analyzer", "Creates facts for OOAnalyzer", AnalyzerType.BYTE_ANALYZER);
 	}
 
 	@Override
 	public boolean getDefaultEnablement(Program program) {
-
-		// TODO: Return true if analyzer should be enabled by default
-
 		return true;
 	}
 
 	@Override
 	public boolean canAnalyze(Program program) {
-
-		// TODO: Examine 'program' to determine of this analyzer should analyze it.  Return true
-		// if it can.
-
 		return true;
 	}
 
 	@Override
 	public void registerOptions(Options options, Program program) {
-
-		// TODO: If this analyzer has custom options, register them here
-
-		options.registerOption("Option name goes here", false, null,
-			"Option description goes here");
+		options.registerOption(FILEPATH_OPTION, OptionType.FILE_TYPE, new File(System.getProperty("user.home") + "\\Documents\\facts.ghidrafacts"), null, "File location where the export is placed.");
+	}
+	
+	@Override
+	public void optionsChanged(Options options, Program program) {
+		exportFile = options.getFile(FILEPATH_OPTION, null);
 	}
 
 	@Override
 	public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log)
 			throws CancelledException {
-
+		
 		var decompService = new GhidraDecompilationAdapter(program);
 		decompService.initialize();
-		
+
 		var graphService = new GhidraDataFlowAdapter(decompService);
 		var factExporter = new FactExporter(decompService, graphService);
-		
-		var fileName = "C:/Users/jeroe/Downloads/" + decompService.decompiledFileName().split(Pattern.quote("."))[0] + ".ghidrafacts";
-		var file = new TextFile(fileName);
+
+		var file = new TextFile(exportFile.getAbsolutePath());
 		factExporter.createFacts(file);
 
 		return false;
--- readme.md
+++ readme.md
@@ -13,7 +13,7 @@ To install Ghidra and the factexporter analyzer follow the [Ghidra installation 
 
 For convenience it is also possible to import the analyzer with the provided 
 
-To run the factexporter you can either import the analyer in Ghidra with [zip file](ghidra_11.0_PUBLIC_20240112_FactExporter.zip).
+To run the factexporter you can either import the analyer in Ghidra with [zip file](ghidra_11.0_PUBLIC_20240312_FactExporter.zip).
 Or you can build the analyzer yourself with Eclipse. For that you need the [GhidraDev](https://ghidra-sre.org/InstallationGuide.html#Development). 
 
 To run the analyzer you can open Ghidra, import the analyzer and run the analyzer on a binary.
@@ -22,3 +22,5 @@ Only Windows x64 and x86 are tested, for now.
 When in Ghidra go to Analysis -> Analyze All Open and make sure you see the fact exporter analyser.
 
 ![Screenshot](images/AnalysisOptions.png)
+
+The option ExportPath enables you to export the facts to a location of your choice.
